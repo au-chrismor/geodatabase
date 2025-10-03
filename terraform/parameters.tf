b61c74604fde92adca1bfd30d7a95754cf0a71de
Christopher Moran <chris.moran@emergent.com.au>
2025-10-03 12:14:50 +1000

    Adding database to Terraform

--- terraform/parameters.tf
+++ terraform/parameters.tf
@@ -1,8 +1,8 @@
-#resource "aws_ssm_parameter" "database_host" {
-#  name = "/${var.project}/${var.version_id}/${var.environment}/dbhost"
-#  type = "String"
-#  value = aws_db_instance.database.address
-#}
+resource "aws_ssm_parameter" "database_host" {
+  name = "/${var.project}/${var.version_id}/${var.environment}/dbhost"
+  type = "String"
+  value = aws_db_instance.database.address
+}
 
 resource "aws_ssm_parameter" "database_user" {
   name = "/${var.project}/${var.version_id}/${var.environment}/dbuser"
@@ -19,7 +19,7 @@ resource "aws_ssm_parameter" "database_password" {
 resource "aws_ssm_parameter" "database_name" {
   name = "/${var.project}/${var.version_id}/${var.environment}/dbname"
   type = "String"
-  value = "geo${var.version_id}"
+  value = "${var.project}${var.version_id}"
 }
 
 resource "aws_vpc_endpoint" "ssm_endpoint" {
